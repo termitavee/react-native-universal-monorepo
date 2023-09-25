bf6bc23d78767cfc58c8333776018a7809615177
Jose Roberto <termitavee@gmail.com>
2023-09-25 19:36:29 +0200

    Successfully load and parse external workspace for web

--- workspace/electron/src/index.tsx
+++ workspace/electron/src/index.tsx
@@ -1,12 +1,18 @@
 import { App } from '@workspace/app/src/App';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import './index.css';
 
-ReactDOM.render(
+
+const container = document.getElementById('root');
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
+
+
