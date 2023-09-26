28aa59d1492b244e7348d75754668f5bc83dbbe4
Jose Roberto <termitavee@gmail.com>
2023-09-26 18:35:47 +0200

    Fix electron builds

--- workspace/electron/craco.config.ts
+++ workspace/electron/craco.config.ts
@@ -1,7 +1,7 @@
 // const CracoLessPlugin = require('craco-less');
 const path = require('node:path');
 
-const webpack = require("webpack");
+const webpack = require('webpack');
 
 module.exports = {
   typescript: {
@@ -9,25 +9,29 @@ module.exports = {
   },
 
   plugins: [
-      // new webpack.DefinePlugin({
-      //   // Inject the "__DEV__" global variable.
-      // "__DEV__": JSON.stringify(process.env.NODE_ENV !== "production"),
-      //   // Inject the "__SUBPLATFORM__" global variable.
-      //   __SUBPLATFORM__: JSON.stringify("electron"),
-      // }),
- {
+    // new webpack.DefinePlugin({
+    //   // Inject the "__DEV__" global variable.
+    // "__DEV__": JSON.stringify(process.env.NODE_ENV !== "production"),
+    //   // Inject the "__SUBPLATFORM__" global variable.
+    //   __SUBPLATFORM__: JSON.stringify("electron"),
+    // }),
+    {
       plugin: {
         overrideWebpackConfig: ({ webpackConfig }) => {
-          const oneOfRule = webpackConfig.module.rules.find(
-            (rule) => rule.oneOf,
+          webpackConfig.plugins.push(
+            new webpack.DefinePlugin({
+              __DEV__: JSON.stringify(process.env.NODE_ENV !== 'production'),
+              __SUBPLATFORM__: JSON.stringify('electron'),
+            })
           );
+
+          // import workspace/app so it's compiled too
+          const oneOfRule = webpackConfig.module.rules.find((rule) => rule.oneOf);
           if (oneOfRule) {
-            const tsxRule = oneOfRule.oneOf.find(
-              (rule) => rule.test && rule.test.toString().includes('tsx'),
-            );
+            const tsxRule = oneOfRule.oneOf.find((rule) => rule.test && rule.test.toString().includes('tsx'));
 
             const newIncludePaths = [
-              // relative path to my workspace library  
+              // relative path to my workspace library
               path.resolve(__dirname, '../../workspace/app/'),
             ];
             if (tsxRule) {
@@ -39,8 +43,7 @@ module.exports = {
             }
           }
           // console.log("webpackConfig",JSON.stringify(webpackConfig))
-
-          return (webpackConfig)
+          return webpackConfig;
         },
       },
     },
--- workspace/electron/src/index.tsx
+++ workspace/electron/src/index.tsx
@@ -4,15 +4,12 @@ import { createRoot } from 'react-dom/client';
 
 import './index.css';
 
-
 const container = document.getElementById('root');
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
-const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
 // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-
